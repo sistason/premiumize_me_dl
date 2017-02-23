39fe65b1625d7b9ced0a454d7e69c5090c1549f9
Kai Sisterhenn <sistason@sistason.de>
2017-02-23 17:30:55 +0100

    Finished mostly.
    ...yes, that's totally how commits work and what they're for!

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # premiumize.me.dl
 Downloads your premiumize.me downloaded files ("My Files") via cli
+
+## Usage
+python3 premiumize_me_download.py files [files, ...] dir [-a auth] [-d]
+ - files: Regular expressions for which files to get
+ - dir: Directory to download files to
+ - -a: Supply authentication information. These can be either:
+   - A string in the format "user:pass"
+   - A file containing "user:pass"
+ - -d: Set to delete successfully downloaded files from premiumize.me
+ 
+ ## Dependencies
+ - python3
+ - python3-requests
+ - python3-tqdm (via pip)
+ - A valid premiumize.me account with Premium ;)
--- premiumize_me_download.py
+++ premiumize_me_download.py
@@ -1,10 +1,131 @@
 #!/usr/bin/env python
+import datetime
+import requests
 import logging
+import zipfile
+import time
+import json
+import sys
+import os
+import re
+from tqdm import tqdm
+
+
+class File:
+    def __init__(self, properties):
+        try:
+            self.id = int(properties.get('id', 0))
+            self.hash = hex(int(properties.get('hash', 0x0), base=16))
+            self.size = int(properties.get('size', 0))
+            self.size_in_mb = int(self.size/1024/1024)
+            self.name = properties.get('name', '')
+            self.created_at = datetime.datetime.fromtimestamp(int(properties.get('created_at', 0)))
+            self.type = properties.get('type', '')
+        except (ValueError, IndexError, AttributeError):
+            del self
+            return
+
+    def __str__(self):
+        return '{s.id}: {s.name} ({s.size}byte)'.format(s=self)
 
 
 class PremiumizeMeDownloader:
-    def __init__(self, *args, **kwargs):
-        pass
+    def __init__(self, download_directory, delete_after_download=False, auth=''):
+        logging.getLogger("requests").setLevel(logging.WARNING)
+        self.delete_after_download = delete_after_download
+        self.download_directory = download_directory
+
+        self.username, self.password = self._read_auth(auth)
+        if not self.username:
+            sys.exit(1)
+        self.login_data = {'customer_id': self.username, 'pin': self.password}
+
+    @staticmethod
+    def _read_auth(auth):
+        if auth and os.path.exists(auth):
+            with open(auth, 'r') as f:
+                auth = f.read()
+
+        if not (auth and ':' in auth):
+            logging.error('No ":" found in authentication information, login not possible!')
+            return None, None
+
+        username, password = auth.strip().split(':')
+        return username, password
+
+    def _make_request(self, url, data=None):
+        """ Do a request, take care of the cookies, timeouts and exceptions """
+        data_ = self.login_data
+        if data is not None:
+            data_.update(data)
+        ret = ''
+        retries = 3
+        while not ret and retries > 0:
+            try:
+                r_ = requests.post(url, data=data_, timeout=2)
+                ret = r_.text
+            except (requests.exceptions.Timeout, requests.exceptions.ConnectionError):
+                retries -= 1
+                time.sleep(1)
+            except Exception as e:
+                logging.debug('Caught Exception "{}" while making a get-request to "{}"'.format(e.__class__, url))
+                break
+        return ret
+
+    def download_files(self, file_regexes):
+        search_re = re.compile(r'{}'.format('|'.join(file_regexes)))
+        files_deleted = []
+        file_list = self._get_list_of_files()
+        for file_ in file_list:
+            if search_re.search(file_.name):
+                success = self._download_file(file_)
+                if success and self.delete_after_download:
+                    self._delete_file(file_)
+                    files_deleted.append(file_)
+
+        if self.delete_after_download:
+            [file_list.remove(d) for d in files_deleted]
+            logging.info('Remaining files in "My Files":  {}'.format(file_list))
+
+    def _get_list_of_files(self):
+        ret = self._make_request('https://www.premiumize.me/api/folder/list')
+        ret_j = json.loads(ret)
+        if 'error' in ret_j:
+            logging.error('Error while getting file-list. Was: {}'.format(ret_j.get('message')))
+            return []
+
+        return [File(properties_) for properties_ in ret_j.get('content', [])]
+
+    def _download_file(self, file_):
+        ret = self._make_request('https://www.premiumize.me/api/torrent/browse?hash={}'.format(str(file_.hash)[2:]))
+        ret_j = json.loads(ret)
+        zip_dl_link = ret_j.get('zip', '')
+
+        logging.info('Downloading {} ({} MB)...'.format(file_.name+'.zip', file_.size_in_mb))
+        return self._download(file_, zip_dl_link)
+
+    def _download(self, file_, link):
+        r = requests.get(link, data=self.login_data, stream=True)
+        if r.ok:
+            file_destination = os.path.join(self.download_directory, file_.name+'.zip')
+            with open(file_destination, 'wb') as f:
+                for data in tqdm(r.iter_content(), total=file_.size, unit='B', unit_scale=True):
+                    f.write(data)
+
+            z = zipfile.ZipFile(file_destination)
+            z.extractall(path=self.download_directory)
+            # os.remove(f)
+            return True
+        else:
+            logging.error('Download of "{}" failed, returned "{}"!'.format(link, r.status_code))
+            return False
+
+    def _delete_file(self, file_):
+        ret = self._make_request('https://www.premiumize.me/api/item/delete?type={}&id={}'.format(file_.type, file_.id))
+        if 'success' in ret:
+            return True
+        logging.error('Could not delete file {}: {}'.format(file_, ret))
+
 
 if __name__ == '__main__':
     import argparse
@@ -13,19 +134,31 @@ if __name__ == '__main__':
     def argcheck_dir(string):
         if path.isdir(string) and access(string, W_OK) and access(string, R_OK):
             return path.abspath(string)
-        raise argparse.ArgumentTypeError('%s is no directory or isn\'t writeable' % string)
+        raise argparse.ArgumentTypeError('{} is no directory or isn\'t writeable'.format(string))
 
-    argparser = argparse.ArgumentParser(description="Download files from your files at premiumize.me")
+    def argcheck_re(string):
+        print(string)
+        try:
+            re.compile(string)
+            return string
+        except re.error:
+            raise argparse.ArgumentTypeError('{} is no valid regular expression!'.format(string))
 
-    argparser.add_argument('files', nargs='*',
-                           help='Download the files with that matching regex.')
+    argparser = argparse.ArgumentParser(description="Download your files at premiumize.me")
+    argparser.add_argument('files', nargs='+', type=argcheck_re,
+                           help='Download all files matching these (python) regular expressions.')
     argparser.add_argument('download_directory', type=argcheck_dir, default='.',
                            help='Set the directory to download the file(s) into.')
     argparser.add_argument('-d', '--delete_after_download', action="store_true",
-                           help="Delete files from My Files after download")
+                           help="Delete files from My Files after successful download")
+    argparser.add_argument('-a', '--auth', type=str,
+                           help="Either 'user:password' or a path to a pw-file with that format")
 
     args = argparser.parse_args()
 
-    dl = PremiumizeMeDownloader(vars(args))
-    logging.basicConfig(format='%(funcName)-23s: %(message)s',
-                        level=logging.DEBUG)
+    logging.basicConfig(format='%(message)s',
+                        level=logging.INFO)
+
+    dl = PremiumizeMeDownloader(args.download_directory,
+                                delete_after_download=args.delete_after_download, auth=args.auth)
+    dl.download_files(args.files)
